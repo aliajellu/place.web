d1b747c7850d33e475c9d70b5d89fa7eef4c4fc7
aliajellu <ali.ajellu@gmail.com>
2012-01-19 14:16:34 -0500

    moved some sql around - messed up after upstream merge

--- _db_sql/changesets/2.sql
+++ _db_sql/changesets/2.sql
@@ -2,9 +2,3 @@ alter table assessment_reviews add column mark_for_discussion bool default 0;
 alter table assessment_reviews add column assessment_id int null after date_created;
 alter table assessment_reviews add foreign key (assessment_id) references assessment(id);
 
-insert into activity_type (run_id, author_id, name) values (1,1,'created journal entry');
-
-ALTER TABLE `question` ADD `allow_multipe_answer` INT NOT NULL DEFAULT '0' AFTER `is_public`;
-
-UPDATE `example` SET type=1;
-
--- _db_sql/changesets/3.sql
+++ _db_sql/changesets/3.sql
@@ -1,3 +1,5 @@
-alter table assessment_reviews add column mark_for_discussion bool default 0;
-alter table assessment_reviews add column assessment_id int null after date_created;
-alter table assessment_reviews add foreign key (assessment_id) references assessment(id);
+insert into activity_type (run_id, author_id, name) values (1,1,'created journal entry');
+
+alter table `question` ADD `allow_multipe_answer` int not null DEFAULT '0' AFTER `is_public`;
+
+update `example` set type=1;
